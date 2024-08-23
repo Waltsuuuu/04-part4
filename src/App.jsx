818f0903e1bcfac46749dbe55b0a3de2455f93c5
Waltsuuu <waltteriheino@gmail.com>
2024-08-23 10:50:41 +0300

    Tapahtumankäsittelijän vieminen alikomponenttiin

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+
+
 const App = (props) => {
   const [value, setValue] = useState(10)
 
-  const hello = (who) => {
-    const handler = () => {
-      console.log('hello', who)
-    }
-    return handler
+  const setToValue = (newValue) => {
+    console.log('value now', newValue) 
+    setValue(newValue)
   }
 
   return (
     <div>
       {value}
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
+
 
 export default App
